357ced99b7a83199ad22272e2082c21008ea3afc
kumar-manjeet <manjeetyadav725@gmail.com>
2023-12-15 20:51:20 +0530

    added space

--- src/main/java/com/tms/models/Query.java
+++ src/main/java/com/tms/models/Query.java
@@ -151,5 +151,7 @@ public class Query {
 				+ ", email=" + email + ", relatedSearch=" + relatedSearch + ", mobileNumber=" + mobileNumber
 				+ ", status=" + status + ", description=" + description + "]";
 	}
+	
+	
 
 }
